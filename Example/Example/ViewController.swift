2bb5ed0f8d54b88202be4358cdf73493a1c0ed10
sungyoungPark <sygitter1491012@gmail.com>
2025-11-22 04:06:14 +0900

    Feat : 예제 추가

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 박성영 on 11/22/25.
+//
+
+import UIKit
+import MediaAccessKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Example/ExampleTests/ExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTests.swift
+//  ExampleTests
+//
+//  Created by 박성영 on 11/22/25.
+//
+
+import Testing
+@testable import Example
+
+struct ExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
