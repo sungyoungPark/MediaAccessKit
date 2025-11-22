7672587898bda481bb788731725002233f40132f
sungyoungPark <sygitter1491012@gmail.com>
2025-11-22 13:39:34 +0900

    Fix : dispatchQueue 버그 수정

--- Sources/MediaAccessKit/MediaAccessKit.swift
+++ Sources/MediaAccessKit/MediaAccessKit.swift
@@ -14,19 +14,20 @@ public struct MediaItem {
 
 @MainActor
 @available(iOS 14, *)
-final class MediaAccessManager: NSObject {
+public final class MediaAccessManager: NSObject {
     public static let shared = MediaAccessManager()
     
     private weak var presentingVC: UIViewController?
     
     // 결과를 전달할 클로저
     private var completion: (([MediaItem]) -> Void)?
-        
-    func presentMediaOptions(from vc: UIViewController,
-                                 allowsMultipleSelection: Bool = true,
-                             alertMessage: String? = nil,
-                             configure: (UIAlertController) -> Void,
-                             completion: (([MediaItem]) -> Void)? = nil) {
+    private let delegate = MediaAccessDelegate()
+    
+    public func presentMediaOptions(from vc: UIViewController,
+                                    allowsMultipleSelection: Bool = true,
+                                    alertMessage: String? = nil,
+                                    configure: (UIAlertController) -> Void,
+                                    completion: (([MediaItem]) -> Void)? = nil) {
         
         let alert = UIAlertController(title: nil, message: alertMessage, preferredStyle: .actionSheet)
         self.presentingVC = vc
@@ -54,14 +55,14 @@ final class MediaAccessManager: NSObject {
             print("카메라 첨부하기")
             self?.permissionCamera()
         }
-     
+        
         alert.addAction(albumBtn)
         alert.addAction(cameraBtn)
         
         DispatchQueue.main.async {
             vc.present(alert, animated: true)
         }
-
+        
     }
     
     private func AuthSettingOpen(AuthString: String) {
@@ -93,14 +94,14 @@ final class MediaAccessManager: NSObject {
     private func AuthPhotoSettingOpen(){
         let message = "앱 설정에서 사진에 대한 액세스 권한을\n '모든 사진' 또는 '선택한 사진'으로\n 변경해주세요."
         let alert = UIAlertController(title: "앨범에서 사진을 첨부하려면 액세스 권한을 부여해야합니다.", message: message, preferredStyle: .alert)
-       
+        
         let cancel = UIAlertAction(title: "허용 안 함", style: .default) { [weak self] (UIAlertAction) in  //취소 클릭했을때 처리해야하는 함수 추가 할것
             print("\(String(describing: UIAlertAction.title)) 클릭")
             
         }
         
         let confirm = UIAlertAction(title: "확인", style: .default) { (UIAlertAction) in
-//                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            //                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
             
             UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:]) { flag in
                 print("flag ====", flag)
@@ -118,9 +119,10 @@ final class MediaAccessManager: NSObject {
 }
 
 // 앨범
+@MainActor
 @available(iOS 14, *)
-extension MediaAccessManager: PHPickerViewControllerDelegate {
- 
+extension MediaAccessManager {
+    
     private func permissionCamera() {
         
         let camera_Authorized = AVCaptureDevice.authorizationStatus(for: .video)
@@ -130,18 +132,18 @@ extension MediaAccessManager: PHPickerViewControllerDelegate {
             print("카메라 권한 처음 물어봄")
             
             AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
-                if granted {
-                    print("카메라 권한 Y 상태")
-                    self?.openCamera()
-                }
-                else {
-                    print("카메라 권한 N 상태")
-                    DispatchQueue.main.async { [weak self] in
+                DispatchQueue.main.async { [weak self] in
+                    if granted {
+                        print("카메라 권한 Y 상태")
+                        self?.openCamera()
+                    }
+                    else {
+                        print("카메라 권한 N 상태")
                         self?.AuthSettingOpen(AuthString: "카메라")
                     }
                 }
             }
-
+            
         case .restricted, .denied:
             print("카메라 권한 물어본 적 있고, 카메라 권한 N 상태일때")
             DispatchQueue.main.async { [weak self] in
@@ -169,22 +171,17 @@ extension MediaAccessManager: PHPickerViewControllerDelegate {
             //photo 권한
             let requiredAccessLevel: PHAccessLevel = .readWrite
             PHPhotoLibrary.requestAuthorization(for: requiredAccessLevel) { [weak self] authorizationStatus in
-                print("확인 ===", authorizationStatus)
-                switch authorizationStatus {
-                case .denied, .notDetermined, .restricted:
-                    print("포토 권한 N 상태")
-                    
-                    DispatchQueue.main.async { [weak self] in
+                DispatchQueue.main.async { [weak self] in
+                    print("확인 ===", authorizationStatus)
+                    switch authorizationStatus {
+                    case .denied, .notDetermined, .restricted:
+                        print("포토 권한 N 상태")
                         self?.AuthPhotoSettingOpen()
-                    }
-                    break
-                    
-                case .limited:
-                    print("포토 권한 limited 상태")
-                    DispatchQueue.main.async {
+                        break
                         
+                    case .limited:
+                        print("포토 권한 limited 상태")
                         let pickerVC = LimitedLibraryPickerViewController()
-
                         pickerVC.onComplete = { [weak self] mediaitems in
                             print("넘어온 이미지", mediaitems)
                             self?.completion?(mediaitems)
@@ -193,26 +190,22 @@ extension MediaAccessManager: PHPickerViewControllerDelegate {
                         let nav = UINavigationController(rootViewController: pickerVC)
                         nav.modalPresentationStyle = .fullScreen // or .pageSheet
                         self?.presentingVC?.present(nav, animated: true)
-                    }
-                    
-                    break
-                case .authorized:
-                    print("포토 권한 Y 상태")
-                    PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: (self?.presentingVC)!)
-                    
-                    DispatchQueue.main.async { [weak self] in
+                        break
+                        
+                    case .authorized:
+                        print("포토 권한 Y 상태")
+                        PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: (self?.presentingVC)!)
                         self?.openAlbum()
+                        break
+                    default:
+                        print("포토 권한 Unimplemented")
                     }
-                    
-                    break
-                default:
-                    print("포토 권한 Unimplemented")
                 }
             }
             
         case .restricted, .denied:
             print("포토 권한을 물어본 적이 있고, 포토 권한 N 상태 (설정 창으로 이동)")
-
+            
             DispatchQueue.main.async { [weak self] in
                 if #available(iOS 14, *) {
                     self?.AuthPhotoSettingOpen()
@@ -221,7 +214,7 @@ extension MediaAccessManager: PHPickerViewControllerDelegate {
                     self?.AuthSettingOpen(AuthString: "사진")
                 }
             }
-//           popUpCameraPermission()
+            
         case .limited:
             DispatchQueue.main.async {
                 let pickerVC = LimitedLibraryPickerViewController()
@@ -239,7 +232,6 @@ extension MediaAccessManager: PHPickerViewControllerDelegate {
             
         case .authorized:
             print("포토 권한을 물어본 적이 있고, 포토 권한 Y 상태")
-//            PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: (self.presentingVC)!)
             openAlbum()
         @unknown default:
             break
@@ -252,7 +244,8 @@ extension MediaAccessManager: PHPickerViewControllerDelegate {
         configuration.filter = .any(of: [.images, .videos]) // 사진 + 동영상
         
         let picker = PHPickerViewController(configuration: configuration)
-        picker.delegate = self
+        picker.delegate = delegate
+        delegate.completion = completion
         
         if let popover = picker.popoverPresentationController {
             guard let presentingVC = presentingVC else { return }
@@ -266,60 +259,9 @@ extension MediaAccessManager: PHPickerViewControllerDelegate {
         presentingVC?.present(picker, animated: true)
     }
     
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        picker.dismiss(animated: true)
-        if results.isEmpty { return }
-        
-        let group = DispatchGroup()
-        var attachments: [MediaItem] = []
-        
-        for result in results {
-            let provider = result.itemProvider
-            
-            // 이미지 처리
-            if provider.canLoadObject(ofClass: UIImage.self) {
-                group.enter()
-                provider.loadObject(ofClass: UIImage.self) { (object, error) in
-                    defer { group.leave() }
-                    if let image = object as? UIImage,
-                       let imageData = image.jpegData(compressionQuality: 0.9) {
-                        let media = MediaItem(data: imageData, mimeType: "image/jpeg", fileName: "photo\(attachments.count).jpg")
-                        attachments.append(media)
-                    }
-                }
-            }
-            
-            // 동영상 처리
-            else if provider.hasItemConformingToTypeIdentifier(UTType.movie.identifier) {
-                group.enter()
-                provider.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier) { (url, error) in
-                    defer { group.leave() }
-                    guard let url = url else { return }
-                    
-                    let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString + ".mov")
-                    do {
-                        try FileManager.default.copyItem(at: url, to: tempURL)
-                        if let videoData = try? Data(contentsOf: tempURL) {
-                            let media = MediaItem(data: videoData, mimeType: "video/quicktime", fileName: "video\(attachments.count).mov")
-                            attachments.append(media)
-                        }
-                    } catch {
-                        print("동영상 복사 실패:", error)
-                    }
-                }
-            }
-        }
-        
-        //모든 파일 로드 완료 시점
-        group.notify(queue: .main) {
-            self.completion?(attachments)
-            self.completion = nil // 메모리 해제 방지
-        }
-    }
-    
 }
 
-extension MediaAccessManager: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+extension MediaAccessManager {
     
     private func openCamera() {
         guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
@@ -328,27 +270,11 @@ extension MediaAccessManager: UIImagePickerControllerDelegate, UINavigationContr
         }
         
         let picker = UIImagePickerController()
-        picker.delegate = self
+        picker.delegate = delegate
+        delegate.completion = completion
+        
         picker.sourceType = .camera
         presentingVC?.present(picker, animated: true)
-    }
-
-    // MARK: - 촬영 완료 시 호출
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        picker.dismiss(animated: true)
-        
-        guard let image = info[.originalImage] as? UIImage else { return }
-        
-        print("촬영 완료 ---", image)
-        let mediaItem = MediaItem(data: image.pngData()!, mimeType: "image/jpeg", fileName: "test.png")
-        completion?([mediaItem])
-        
-    }
-    
-    // MARK: - 촬영 취소 시
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true)
-        print("취소!!")
     }
     
 }
